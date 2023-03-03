d9b75fed637dbf70153ad3cd933c4d1a4a7b538d
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-03-03 09:08:21 +0300

    Store all the Enum into one place

---
+++ flash-cards-bot/const.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from enum import Enum
+
+
+class MenuButtons(str, Enum):
+    WORK_WITH_SETS = 'Работа с сетами'
+    TESTS = 'Тесты'
+    INPUT_TESTS = 'Тесты с вводом'
+
+
+class Markups(str, Enum):
+    MENU = 'menu'
+    SET = 'set'
+
+
+class SetButtons(str, Enum):
+    NEW_SET = 'Создать новый сет'
+
+
+class Commands(str, Enum):
+    START = 'start'
+    MENU = 'menu'
+    HELP = 'help'
+
+
+class States(str, Enum):
+    INPUT_SET_NAME = 'Введи номер сета'
+
+
+CURRENT_STATE = None
