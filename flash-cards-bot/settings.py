fecf3d30ef556bfb93cfa5704bcd3da05e10d331
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-03-03 10:27:02 +0300

    Added settings for application

--- flash-cards-bot/settings.py
+++ flash-cards-bot/settings.py
@@ -1,7 +1,34 @@
 from __future__ import annotations
 
+from pathlib import Path
+from typing import NamedTuple
+
 
 with open('.env') as f:
-    lines = f.readlines()
-    TOKEN = lines[0].split('=')[-1].strip()
-    ADMIN_ID = lines[1].split('=')[-1].strip()
+    env_dict = {}
+    for line in f:
+        line = line.strip()
+        if line.startswith('#') or not line:
+            continue
+        key, value = line.split('=', 1)
+        env_dict[key] = value
+
+
+_BASE_DIR = Path(__file__)
+_DB_DIR = _BASE_DIR / 'db'
+_conn_str = 'sqlite:///' + (_DB_DIR / 'flash.sqlite').as_posix()
+
+
+class Settings(NamedTuple):
+    admin_id: str
+    token: str
+    base_dir: Path
+    conn_str: str
+
+
+settings = Settings(
+        admin_id=env_dict['ADMIN_ID'],
+        token=env_dict['FLASH_BOT_TOKEN'],
+        conn_str=_conn_str,
+        base_dir=_BASE_DIR,
+    )
