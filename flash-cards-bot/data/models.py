82d085ae126e54b31f21c239114dbef4470f7703
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-03-03 09:50:09 +0300

    Write models for sets and cards

---
+++ flash-cards-bot/data/models.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import datetime as dt
+
+import sqlalchemy as sa
+from data.basemodel import Base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+# import uuid
+
+
+class Set(Base):
+    __tablename__ = 'sets'
+
+    # # Try to use name as primary_key
+    # id: Mapped[str] = mapped_column(
+    #         primary_key=True,
+    #         default=lambda: str(uuid.uuid4()).replace('-', ''),
+    #     )
+    name: Mapped[str] = mapped_column(primary_key=True)
+    cdate: Mapped[dt.datetime] = mapped_column(
+            sa.DateTime,
+            default=dt.datetime.utcnow,
+        )
+    udate: Mapped[dt.datetime] = mapped_column(
+            sa.DateTime,
+            default=dt.datetime.utcnow,
+        )
+
+
+class Card(Base):
+    __tablename__ = 'cards'
+
+    term: Mapped[str] = mapped_column(
+            primary_key=True,
+        )
+    definition: Mapped[str] = mapped_column(sa.String, nullable=True)
+    cdate: Mapped[dt.datetime] = mapped_column(
+            sa.DateTime,
+            default=dt.datetime.utcnow,
+        )
+    udate: Mapped[dt.datetime] = mapped_column(
+            sa.DateTime,
+            default=dt.datetime.utcnow,
+        )
