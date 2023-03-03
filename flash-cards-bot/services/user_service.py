4a77adb64a1cdd0223719d414461fda587783c7a
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-03-03 12:27:59 +0300

    Update user state service

--- flash-cards-bot/services/user_service.py
+++ flash-cards-bot/services/user_service.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import datetime as dt
 
+from const import States
 from data import db_session
 from data.models import User
 from sqlalchemy.orm import Session
@@ -14,6 +15,20 @@ def init_user(
     user = session.query(User).where(User.id == user_id).one_or_none()
     if not user:
         user = User(id=user_id)
+    user.udate = dt.datetime.utcnow()
+    session.add(user)
+    session.commit()
+    return user
+
+
+def update_state(
+        user_id: int,
+        state: States,
+) -> User:
+    session: Session = db_session.create_session()
+    user = session.query(User).where(User.id == user_id).one()
+
+    user.state = state
     user.udate = dt.datetime.utcnow()
     session.add(user)
     session.commit()
