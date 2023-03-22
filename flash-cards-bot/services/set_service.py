b02906466c9e28405656bc4b8162081409b8ae55
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2023-03-22 10:48:07 +0300

    Session as argument for services

--- flash-cards-bot/services/set_service.py
+++ flash-cards-bot/services/set_service.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from data import db_session
 from data.models import Set
 from sqlalchemy.orm import Session
 
@@ -8,8 +7,8 @@ from sqlalchemy.orm import Session
 def new_set(
         name: str,
         user_id: int,
+        session: Session,
 ) -> Set:
-    session: Session = db_session.create_session()
     set_ = Set(name=name, user_id=user_id)
     session.add(set_)
     session.commit()
--- flash-cards-bot/services/user_service.py
+++ flash-cards-bot/services/user_service.py
@@ -3,19 +3,19 @@ from __future__ import annotations
 import datetime as dt
 
 from const import States
-from data import db_session
 from data.models import User
 from sqlalchemy.orm import Session
 
 
 def init_user(
         user_id: int,
+        state: States,
+        session: Session,
 ) -> User:
-    session: Session = db_session.create_session()
-
     user = session.query(User).where(User.id == user_id).one_or_none()
     if not user:
         user = User(id=user_id)
+        user.state = state
     user.udate = dt.datetime.utcnow()
     session.add(user)
     session.commit()
@@ -25,9 +25,8 @@ def init_user(
 def update_state(
         user_id: int,
         state: States,
+        session: Session,
 ) -> User:
-    session: Session = db_session.create_session()
-
     user = session.query(User).where(User.id == user_id).one()
     user.state = state.value
     user.udate = dt.datetime.utcnow()
@@ -38,7 +37,6 @@ def update_state(
 
 def get_user_by_id(
         user_id: int,
+        session: Session,
 ) -> User:
-    session: Session = db_session.create_session()
-
     return session.query(User).where(User.id == user_id).one()
